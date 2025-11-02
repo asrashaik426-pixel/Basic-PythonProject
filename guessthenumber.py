f4e61a9a7af98074d7276e9953729982b2c34b0e
Asra Shaik <asrashaik426@gmail.com>
2025-11-02 20:29:10 +0530

    Add files via upload

---
+++ guessthenumber.py
@@ -0,0 +1,15 @@
+import random
+print("Let's pay a guessing game with me to pass time!")
+random_number=random.randint(1,100)
+guess_attempts=0
+while True:
+    guess=int(input("Close your eyes and think of random between 1 to 100 number:"))
+    guess_attempts=+1
+    if guess==random_number:
+        print("Yay!you got it right")
+        break
+    elif guess<random_number:
+            print("Try for bigger number")
+    else:
+        print("Try for lowwer number ")
+                
